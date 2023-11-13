f4e7577b0ebedcf3ee480b01fde0ffcc52037f6f
kun510 <cuongtran8c@gmail.com>
2023-11-13 23:31:57 +0700

    delete all

--- src/main/java/com/example/doancuoiky/hostel/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.doancuoiky.hostel.repository;
-
-import com.example.doancuoiky.hostel.model.Rent;
-import com.example.doancuoiky.hostel.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
--- src/main/java/com/example/doancuoiky/hostel/request/areaRq.java
+++
@@ -1,8 +0,0 @@
-package com.example.doancuoiky.hostel.request;
-
-import lombok.Data;
-
-@Data
-public class areaRq {
-    private String area;
-}
